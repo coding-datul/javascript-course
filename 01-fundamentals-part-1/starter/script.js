30cd37c41319f4d52ce6b8810bc27fabe55e25d9
coding-datul <johnherszel2@gmail.com>
2025-10-17 23:17:25 +0800

    feat: add basic variables and data types
    - Set up JavaScript environment and browser console
    - Implement let, const, var variable declarations
    - Add data type exploration with typeof operator
    - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,72 @@
-// JavaScript Fundamentals - Part 1
-// We'll write our code here!
+// // // // JavaScript Fundamentals - Part 1
+// // // // We'll write our code here!
+
+    let js = "amazing";
+    console.log(40+8+23-10);
+    console.log(js);
+
+     console.log("=== VARIABLES ===");
+
+    let firstName = "Jonas";
+    console.log(firstName);
+ 
+    let age = 30;
+     console.log(age);
+     age = 50; 
+     console.log(age);
+
+    const birthYear = 1991;
+     console.log(birthYear);
+
+    birthYear = 2003;
+
+     const PI = 3.1415;
+     console.log(PI);
+
+     var job = "programmer";
+     job = "teacher";
+     console.log(job);
+
+     const country = "Philippines";
+     const language = "Filipino";
+
+     age = 25;
+     age = 26; // this will change
+
+// // // // good variable name
+// // // // let firstName 
+// // // // let myCurrentJob 
+// // // // const PI
+
+// // // // bad  name
+// // // // let 3years
+// // // // let jonas&matilda
+// // // // let now
+    
+console.log("=== DATA TYPES ===");
+
+// // // // Number
+     let id = 12345;
+     console.log(id);
+     console.log(typeof age);
+
+     let lastName = "Datul";
+     console.log(lastName);
+     console.log(typeof lastName);
+
+     let javascriptIsFun = true;
+    console.log(javascriptIsFun);
+     console.log(typeof javascriptIsFun);
+
+     let year;
+     console.log(year); 
+     console.log(typeof year);
+
+     let dynamicVariable =40
+     console.log(dynamicVariable, typeof dynamicVariable);
+
+    dynamicVariable = "I am now a String!"
+     console.log(dynamicVariable, typeof dynamicVariable);
+
+    dynamicVariable = true;
+     console.log(dynamicVariable, typeof dynamicVariable);
